940251eada64284c545d54749a646dc1bc7ebdbb
lkmvip <35647095@qq.com>
2020-07-29 22:17:37 +0800

    add option

--- content_script.js
+++ content_script.js
@@ -11,14 +11,16 @@ urls = [
 ]
 
 // very messy workaround for accessing chrome storage outside of background / content scripts
-chrome.storage.sync.get(['useallSub', 'useddplus', 'setMaxBitrate'], function(items) {
+chrome.storage.sync.get(['useallSub', 'useddplus', 'useAVC', 'setMaxBitrate'], function(items) {
     var useallSub = items.useallSub;
 	var useddplus = items.useddplus;
+	var useAVC = items.useAVC;
     var setMaxBitrate = items.setMaxBitrate;
     var mainScript = document.createElement('script');
     mainScript.type = 'application/javascript';
     mainScript.text = 'var useallSub = ' + useallSub + ';' + '\n' 
 	                + 'var useddplus = ' + useddplus + ';' + '\n' 
+					+ 'var useAVC = ' + useAVC + ';' + '\n' 
 	                + 'var setMaxBitrate = ' + setMaxBitrate + ';';
     document.documentElement.appendChild(mainScript);
 });
--- pages/options.js
+++ pages/options.js
@@ -1,10 +1,12 @@
 function save_options() {
 	var useallSub = document.getElementById('allSub').checked;
 	var useddplus = document.getElementById('ddplus').checked;
+	var useAVC = document.getElementById('AVC').checked;
     var setMaxBitrate = document.getElementById('setMaxBitrate').checked;
     chrome.storage.sync.set({
         useallSub: useallSub,
 		useddplus: useddplus,
+		useAVC: useAVC,
         setMaxBitrate: setMaxBitrate
     }, function() {
         var status = document.getElementById('status');
@@ -19,10 +21,12 @@ function restore_options() {
     chrome.storage.sync.get({
         useallSub: false,
 		useddplus: false,
+		useAVC: false,
         setMaxBitrate: false
     }, function(items) {
         document.getElementById('allSub').checked = items.useallSub;
 		document.getElementById('ddplus').checked = items.useddplus;
+		document.getElementById('AVC').checked = items.useAVC;
         document.getElementById('setMaxBitrate').checked = items.setMaxBitrate;
     });
 }
