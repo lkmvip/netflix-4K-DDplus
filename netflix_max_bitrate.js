e74dff7566f14d4100fe4495a3901f17772fbae1
lkmvip <12974141+lkmvip@users.noreply.github.com>
2021-07-25 11:26:37 +0800

    Update netflix_max_bitrate.js
    optimization

--- netflix_max_bitrate.js
+++ netflix_max_bitrate.js
@@ -1,59 +1,64 @@
 let getElementByXPath = function (xpath) {
-  return document.evaluate(
-    xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
-  ).singleNodeValue;
+	return document.evaluate(
+		xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
+	).singleNodeValue;
 };
 
 let fn = function () {
-  const VIDEO_SELECT = getElementByXPath("//div[text()='Video Bitrate']");
-  const AUDIO_SELECT = getElementByXPath("//div[text()='Audio Bitrate']");
-  const BUTTON = getElementByXPath("//button[text()='Override']");
-	 if (!BUTTON) {
-  window.dispatchEvent(new KeyboardEvent('keydown', {
-    keyCode: 83,
-    ctrlKey: true,
-    altKey: true,
-    shiftKey: true,
-  }));
-}
+	const VIDEO_SELECT = getElementByXPath("//div[text()='Video Bitrate']");
+	const AUDIO_SELECT = getElementByXPath("//div[text()='Audio Bitrate']");
+	const BUTTON = getElementByXPath("//button[text()='Override']");
+	
+	if (!(VIDEO_SELECT && AUDIO_SELECT && BUTTON)){
+		window.dispatchEvent(new KeyboardEvent('keydown', {
+			keyCode: 83,
+			ctrlKey: true,
+			altKey: true,
+			shiftKey: true,
+		}));
+		return false;
+	}
 
+	[VIDEO_SELECT, AUDIO_SELECT].forEach(function (el) {
+		let parent = el.parentElement;
 
-  if (!(VIDEO_SELECT && AUDIO_SELECT && BUTTON)){
-    return false;
-  }
+		let options = parent.querySelectorAll('select > option');
 
-  [VIDEO_SELECT, AUDIO_SELECT].forEach(function (el) {
-    let parent = el.parentElement;
+		for (var i = 0; i < options.length - 1; i++) {
+			options[i].removeAttribute('selected');
+		}
 
-    let options = parent.querySelectorAll('select > option');
+		options[options.length - 1].setAttribute('selected', 'selected');
+	});
+	//console.log("Closed!");
+	BUTTON.click();
 
-    for (var i = 0; i < options.length - 1; i++) {
-      options[i].removeAttribute('selected');
-    }
+	return true;
+};
 
-    options[options.length - 1].setAttribute('selected', 'selected');
-  });
-
-  BUTTON.click();
-
-  return true;
+let crash = function (count) {
+    //console.log("menu searching");
+    const BUTTON = getElementByXPath("//button[text()='Override']");
+	if (count > 0) {
+		BUTTON ? fn() : setTimeout(() => crash(count - 1), 200)
+	}
 };
 
 let run = function () {
-  fn() || setTimeout(run, 2000);
+	fn() ? crash(10) : setTimeout(run, 100)
 };
 
 const WATCH_REGEXP = /netflix.com\/watch\/.*/;
 
 let oldLocation;
 if(setMaxBitrate ) {
-  console.log("netflix_max_bitrate.js enabled");
-  setInterval(function () {
-    let newLocation = window.location.toString();
+	console.log("netflix_max_bitrate.js enabled");
+	setInterval(function () {
+		let newLocation = window.location.toString();
 
-    if (newLocation !== oldLocation) {
-      oldLocation = newLocation;
-      WATCH_REGEXP.test(newLocation) && run();
-    }
+		if (newLocation !== oldLocation) {
+			oldLocation = newLocation;
+			WATCH_REGEXP.test(newLocation) && run();
+		}
   }, 500);
 }
